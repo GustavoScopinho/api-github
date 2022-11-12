f84a971e0373ce27ba9f5214e50778626de8387b
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-11-12 16:19:32 -0300

    Add UsuarioApi na page

--- src/pages/Usuario.tsx
+++ src/pages/Usuario.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import RepoAPI from '../components/RepoAPI/RepoAPI'
+import { UsuarioAPI } from '../components/UsuarioAPI/UsuarioAPI'
 
 export const Usuario:React.FC = () => {
   return (
     <>
+    <UsuarioAPI/>
     <RepoAPI/>
     </>
   )
