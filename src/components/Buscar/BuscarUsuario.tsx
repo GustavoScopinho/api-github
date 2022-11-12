b0f23ea85bf27e191e41b864199656d27a239227
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-11-12 17:01:48 -0300

    Buscando nome de usuario

--- src/components/Buscar/BuscarUsuario.tsx
+++ src/components/Buscar/BuscarUsuario.tsx
@@ -2,11 +2,36 @@ import axios from "axios"
 import { useEffect, useState} from 'react'
 import { ContainerBusca, ContainerTexto, ContainerPesquisar, ContainerGeral } from "./Buscar.style"
 
+// interface Usuario {
+//   //   name: string;
+//   //   bio: string;
+//   //   location: string;
+//   //   followers: string;
+//   //   following: string;
+//   //   avatar_url: string;
+//         usuarios: string;
+//   }
+
 
 
 
 export const BuscarUsuario:React.FC = () => {
 
+  const [usuarios, setUsuarios] = useState('')
+
+  const BuscarApi = () => {
+    
+    // let urlWeb = `https://api.github.com/users/${usuarios}`
+    // let token = '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';
+    // axios.get(urlWeb, { headers: {token} } );
+
+    
+    axios.get(`https://api.github.com/users/${usuarios}`)
+    .then(response => {
+      console.log(response.data)
+    })
+    
+  }
 
   return (
     <>
@@ -18,8 +43,8 @@ export const BuscarUsuario:React.FC = () => {
               e veja informações sobre seu perfil</h2>
           </ContainerTexto>
           <ContainerPesquisar>
-          <input type="text" placeholder="username"  id="" />
-          <button type="button">Enviar</button>
+          <input type="text" onChange={(e) => setUsuarios(e.target.value)} placeholder="username"  id="" />
+          <button type="button" onClick={BuscarApi}>Enviar</button>
           </ContainerPesquisar>
           
         </ContainerBusca>
--- src/components/UsuarioAPI/UsuarioAPI.tsx
+++ src/components/UsuarioAPI/UsuarioAPI.tsx
@@ -4,32 +4,9 @@ import { ContainerUsuario, ContainerPerfilUsuario} from './Usuario.style'
 
 
 
-interface Usuario {
-  name: string;
-  bio: string;
-  location: string;
-  followers: string;
-  following: string;
-  avatar_url: string;
-}
+
 
 export const UsuarioAPI:React.FC = () => {
-
-  const nome_usuario = 'gustavoscopinho'
-  let urlWeb = `https://api.github.com/users/${nome_usuario}`;
-  let token = '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';
-  axios.get(urlWeb, { headers:  {token} } );
-
-  const [usuarios, setUsuarios] = useState<Usuario>()
-  
-  useEffect(() => {
-    axios.get(urlWeb)
-    .then(response => {
-      setUsuarios(response.data)
-    })
-  }, [])
-
-
 
 
   return (
